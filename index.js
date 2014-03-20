164de5b24fd65bbad88ae7f512b03cca6b8bd323
Emil Rossing <emil@lemur.dk>
2014-03-20 17:16:14 +0100

    added REST API

--- index.js
+++ index.js
@@ -1,48 +1,59 @@
 var express = require("express");
 var logfmt = require("logfmt");
 var mongo = require('mongodb');
-
 var app = express();
-
-var mongoUri = 	process.env.MONGOLAB_URI ||
-				process.env.MONGOHQ_URL ||
-				'mongodb://localhost/mydb';
-
+var mongoUri = 	process.env.MONGOLAB_URI || process.env.MONGOHQ_URL || 'mongodb://localhost/mydb';
 
 app.use(logfmt.requestLogger());
 
 /******************************** URL routes ********************************/
-
-/**
- * HTTP GET /
- * Param: :
- * Returns:
- * Error: 
- */
 app.get('/', function(req, res) {
-  ins({'pill_red':'sweetness', 'pill_blue':'sour'}); //testing..
-  res.send('Hello Worldy!');
+  //ins({'pill_red':'sweetness', 'pill_blue':'sour'}); //uncomment to insert (confusing) test pill..
+  mongo.Db.connect(mongoUri, function (err, db) {
+	  db.collection('pills', function(er, collection) {
+		 collection.count(function(err, count) {
+			
+			collection.findOne({"pill_blue":"sour"}, function(err, pill) {
+				res.send("We have " + count + " pill"+ (count!=1?"s":"") +". Here's one: " + pill._id);
+			});
+		});
+		 
+	  });
+  });
+  //res.send('Hello Worldy!');
 });
 
+/******************************** REST API ********************************/
+
 /**
- * HTTP GET /red-pills/:id
+ * HTTP GET /:color/:id
  * Param: :id is the unique identifier of the pill you want to eat
+ * Param: :color is the color of the pill you want to eat (red = truth, blue = something else)
  * Returns: a string
  * Error: 404 HTTP code if the pill doesn't exists
  */
-app.get('/red-pills/:id', function (request, response) {
-    var pillId = request.params.id;
-    try {		
-        get(pillId, response);
+app.get('/pills/:color/:id', function (request, response) {
+    try {
+		var pillId = request.params.id;
+		var pillColor = request.params.color;
+		var isRed = ('red'===pillColor.toString().toLowerCase());	
+		if (isRed||('blue'===pillColor.toString().toLowerCase())){ 
+			//response.send(pillColor.toLower());
+			get(pillId, response, isRed);		
+		}else{
+			response.send(404);
+		}		
     } catch (exeception) {
         response.send(404);
     }     
 });
 
 
+
 /**
  * HTTP POST /pills/
  * Body Param: the JSON pill you want to create
+ * {'pill_red':string, 'pill_blue':string}
  * Returns: 200 HTTP code
  */
 app.post('/pills', function (request, response) {
@@ -61,12 +72,21 @@ function ins(pill) {
 	  });
 	});
 }
-//oid = "532ae43f6147350200ea470a"
-function get(oid, response) {
+
+function get(oid, response, red) {
 	mongo.Db.connect(mongoUri, function (err, db) {
 		db.collection('pills', function(er, collection) {
-			collection.findOne({"_id": {"$oid": oid}}, function(err, document) {
-				response.send(document);
+			collection.findOne({"_id":mongo.BSONPure.ObjectID.createFromHexString(oid)}, function(err, pill) {
+			//collection.findOne({"pill_blue":"sour"}, function(err, pill) {
+				if (err || !pill) {
+					response.send(404);
+				} else {				
+					collection.remove({"_id":mongo.BSONPure.ObjectID.createFromHexString(oid)}, function(dele_err, res) {
+						response.send((red)? pill.pill_red : pill.pill_blue);									
+						//response.json(pill);					
+					});
+					db.close();					
+				}
 			});		
 		});
 	});    
