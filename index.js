8297416f02252e95c4ffc67d3fbfaa9e5f55824c
Emil Rossing <emil@lemur.dk>
2014-03-20 13:23:49 +0100

    added mongo

--- index.js
+++ index.js
@@ -1,11 +1,25 @@
 var express = require("express");
 var logfmt = require("logfmt");
+var mongo = require('mongodb');
+
 var app = express();
+
+var mongoUri = 	process.env.MONGOLAB_URI ||
+				process.env.MONGOHQ_URL ||
+				'mongodb://localhost/mydb';
+
 
 app.use(logfmt.requestLogger());
 
 app.get('/', function(req, res) {
-  res.send('Hello World!');
+	mongo.Db.connect(mongoUri, function (err, db) {
+	  db.collection('pills', function(er, collection) {
+		collection.insert({'mykey': 'myvalue'}, {safe: true}, function(er,rs) {
+		});
+	  });
+	});
+
+  res.send('Hello Worldy!');
 });
 
 var port = Number(process.env.PORT || 5000);
